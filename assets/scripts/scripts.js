148e27658377c4ee7a30851fa1fc5022e37a6729
vkuth <vkuth@mail.ru>
2022-10-07 13:24:10 +0600

    feat: add burger menu

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,9 +1,13 @@
 let menuBtn = document.querySelector('.burg_menu');
+let back =document.querySelector('.background');
 let menu = document.querySelector('.menu');
+let close =document.querySelector('.close_btn');
 document.addEventListener('click', function (e) {
   if(menuBtn.contains(e.target)){
     menu.classList.add('active');
-  } else if(!menu.contains(e.target)){
+    back.classList.add('active');
+  } else if(!menu.contains(e.target)|| close.contains(e.target)){
     menu.classList.remove('active');
+    back.classList.remove('active');
   }
 });
